6f0fc9b5edd37504e8d948fc68af7641c78bdb30
varoblakjan <44274112+varoblakjan@users.noreply.github.com>
2018-10-20 02:06:12 +0300

    Create hacker.py

---
+++ hacker.py
@@ -0,0 +1,21 @@
+#create python3
+#Author B3avers
+#
+import urllib.requst as urllib2
+import re
+import sys,os
+import random
+
+H = '\033[95m'
+B = '\033[94m'
+G = '\033[92m'
+W = '\033[93m'
+F = '\033[91m'
+E = '\033[0m'
+U = '\033[4m'
+O = '\033[33m'
+
+def info():
+VERSION = B+'3.0'+E
+AUTHOR = B+'varoblakjan'+E
+print("""
